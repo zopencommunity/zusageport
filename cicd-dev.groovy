0378c341e55c46967c1fe50e28e4b9fb8dfa7d7d
sachint <32639496+sachintu47@users.noreply.github.com>
2025-01-07 18:36:54 +0530

    Review comments fix (#2)
    * Add zopen check result
    
    * Modify cicd groovy file to monitor zedc_ascii src repo
    
    * Review comments fix

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -7,6 +7,13 @@ node('linux')
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
       userRemoteConfigs: [[url: 'https://github.com/zopencommunity/zedc_asciiport.git']]])
+
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/zedc_ascii.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zedc_asciiport.git'), string(name: 'PORT_DESCRIPTION', value: 'The zlib data compression library provides in-memory compression and decompression functions, including integrity checks of the uncompressed data. A modified version of the zlib compression library is used by zEDC. The IBM-provided zlib compatible C library provides a set of wrapper functions that use zEDC compression when appropriate and when zEDC is not appropriate, software-based compression services are used.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
